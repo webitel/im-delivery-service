ef183a51c500213b80753a1939de50710c08c31c
Dtsnko <right0346@gmail.com>
2026-01-30 12:25:44 +0200

    fix(proto/gen): update paths in buf.gen.delivery.yaml and delivery_gen.go for correct module structure

--- buf/delivery_gen.go
+++ buf/delivery_gen.go
@@ -1,3 +1,3 @@
 package buf
 
-//go:generate buf generate ../../protos/im --template buf.gen.delivery.yaml --path ../../protos/im/delivery/v1
+//go:generate buf generate ../../protos/im --template buf.gen.delivery.yaml --path ../../protos/im/api/delivery
